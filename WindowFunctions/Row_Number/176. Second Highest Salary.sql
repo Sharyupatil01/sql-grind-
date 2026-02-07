1b80ac7ec17f57c57bcdb0cf89cf1c1ebb7b2a5a
Sharyupatil01 <sharyu2701@gmail.com>
2026-02-07 10:56:44 +0530

    Added Window Function Folder

---
+++ WindowFunctions/Row_Number/176. Second Highest Salary.sql
@@ -0,0 +1,72 @@
+
+-- row_number() is a window function that assigns a unique sequential integer to rows within a partition of a result set, starting at 1 for the first row in each partition.
+
+
+
+Table: Employee
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains information about the salary of an employee.
+ 
+
+Write a solution to find the second highest distinct salary from the Employee table. If there is no second highest salary, return null (return None in Pandas).
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+Example 2:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| null                |
++---------------------+
+
+
+-- window functions 
+
+-- row_number() is a window function that assigns a unique sequential integer to rows within a partition of a result set, starting at 1 for the first row in each partition.
+
+SELECT 
+     max(salary) as SecondHighestSalary
+FROM (
+    SELECT salary, ROW_NUMBER() OVER (ORDER BY salary DESC) AS rn
+
+    FROM (
+        SELECT DISTINCT salary
+        FROM Employee
+    ) x  
+) t
+WHERE rn = 2;
